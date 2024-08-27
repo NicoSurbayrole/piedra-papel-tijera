e9fd7d7fbbc2a100b2da0d617047fe84cf75f1c8
Nicolas <nico.surba@hotmail.com>
2024-08-26 22:11:56 -0300

    "add audio from win,lose or draw"

--- src/components/Result.tsx
+++ src/components/Result.tsx
@@ -1,5 +1,8 @@
 import { Status } from "../types/status";
 import { STATUS } from "../constantes/status";
+import winnerAudio from "../sonidos/winner.mp3"
+import loserAudio from "../sonidos/loser.mp3"
+import drawAudio from "../sonidos/draw.mp3"
 
 interface ResultProps {
     status: Status | null;
@@ -11,11 +14,27 @@ interface ResultProps {
 export const Result: React.FC<ResultProps> = ({ status }) => {
     switch (status) {
         case STATUS.WIN:
-            return <span className="block text-3xl text-center">Ganaste</span>
+            return (
+                <>
+                    <span className="block text-3xl text-center">Ganaste</span>
+                    <audio typeof="audio/mpeg" src={`${winnerAudio}`} autoPlay></audio>
+                </>
+            )
         case STATUS.LOSE:
-            return <span className="block text-3xl text-center">Perdiste</span>
+            return (
+                <>
+                    <span className="block text-3xl text-center">Perdiste</span>
+                    <audio typeof="audio/mpeg" src={`${loserAudio}`} autoPlay></audio>
+                </>
+
+            )
         case STATUS.DRAW:
-            return <span className="block text-3xl text-center">Empataste</span>
+            return (
+                <>
+                    <span className="block text-3xl text-center">Empataste</span>
+                    <audio typeof="audio/mpeg" src={`${drawAudio}`} autoPlay></audio>
+                </>
+            )
         default:
             return null;
     }
